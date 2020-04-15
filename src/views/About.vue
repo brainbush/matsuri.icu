1fed3d78e5b64aa36c5f053dea990a1f36acbeb8
brainbush <h@qaq.gd>
2020-04-15 22:12:49 +0800

    在关于里面加了一些新的东西……

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,10 +8,15 @@
         </div>
         <div class="container">
             <p>一个非常Anti的站用来采集各位观众的评论进行统计<s>特定观众</s>（一不小心致敬了<a target="_blank" rel="noopener noreferrer"
-                                                   href="https://mamedaifuku.sakura.ne.jp/live_stream/html/index.html">豆大福</a>的样子）<s>明明功能比人家少那么多</s>。
+                                                              href="https://mamedaifuku.sakura.ne.jp/live_stream/html/index.html">豆大福</a>的样子）<s>明明功能比人家少那么多</s>。
             </p>
             <p>如果觉得方便请随意传播及宣传！</p>
-            <p>一般情况下直播间下播后1分钟左右都会更新，如果遇到问题请联系邮箱 <code>h@qaq.gd</code> 或者B站私信ID <code>186629</code>。</p>
+            <p>一般情况下直播间下播后1分钟左右都会更新，如果遇到问题请联系邮箱 <code>h@qaq.gd</code> 或者B站私信ID <a target="_blank"
+                                                                                  rel="noopener noreferrer"
+                                                                                  href="https://message.bilibili.com/#whisper/mid186629"><code>186629</code></a>
+                或者 Telegram: <a
+                        target="_blank" rel="noopener noreferrer" href="https://t.me/brainbush"><code>@brainbush</code></a>。
+            </p>
             <p>目前各种完善中可能遇到不稳定的情况请见谅！</p>
             <p>如果有需要增加收录的也可以通过上面的方式联系！</p>
             <p><s>在每一个直播记录内点击观众的名字可以看到这个前辈发的所有内容（处刑感）</s></p>
@@ -21,6 +26,9 @@
             <p>然后就这样愉快的做出来了，然后貌似还是半自动不太好用<s>以及一些奇奇怪怪的Bug</s>，干脆就一条龙服务吧！</p>
             <p>刚好碰到新型冠状病毒导致各种开学推迟就有足够的时间来做这玩意儿啦！</p>
             <p>也不知道这小破站能持续多久呢……（数据库压力感）</p>
+            <p>貌似加了一些奇奇怪怪的功能，不知道以后会怎样。
+                <del>其实一直搞不明白阿B的礼物分成到底是怎么算的</del>
+            </p>
         </div>
     </div>
 </template>
