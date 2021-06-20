0adcf7f88c87ec555183af64355c357b8cdcc4bc
brainbush <960821@gmail.com>
2021-06-20 18:23:30 +0800

    rolling pages

--- src/views/Viewer.vue
+++ src/views/Viewer.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div v-for="clip in display_result" :key="clip.clip_info.id">
+        <div v-for="clip in data" :key="clip.clip_info.id">
             <ClipList :clip="clip.clip_info" :detail_view="true" :viewer_view="true" :webp_support="webp_support"/>
             <div class="container comment-container">
                 <LiveComment v-for="(comment,index) in clip.full_comments" :key="index" :comment="comment"
@@ -15,52 +15,59 @@
 </template>
 
 <script>
-    import ClipList from "@/components/ClipList";
-    import LiveComment from "@/components/LiveComment";
+import ClipList from "@/components/ClipList";
+import LiveComment from "@/components/LiveComment";
 
-    export default {
-        name: "Viewer",
-        components: {ClipList, LiveComment},
-        data() {
-            return {
-                id: this.$route.params.id,
-                data: [],
-                showed: 20,
-                webp_support: this.$parent.webp_support
-            }
-        },
-        mounted() {
-            document.title = 'ICU for Viewers'
-            this.$parent.loading = true;
-            window.addEventListener('scroll', this.scrollFunc);
-            this.$http
-                .get('https://api.matsuri.icu/viewer/' + this.id)
-                .then(function (response) {
-                    if (response.data.status === 0) {
-                        this.data = response.data.data;
-                        if (this.data.length > 0) document.title = this.data[0].full_comments[0].username + ' - ICU for Viewers';
-                        this.$parent.loading = false;
-                    }
-                }.bind(this))
-        },
-        computed: {
-            display_result: function () {
-                return this.data.filter((clip, index) => index < this.showed)
-            }
-        },
-        methods: {
-            scrollFunc() {
-                if (document.body.clientHeight - window.scrollY - window.innerHeight < (document.body.clientHeight / this.showed)) {
-                    if (this.showed < this.data.length)
-                        this.showed += 10;
+export default {
+    name: "Viewer",
+    components: {ClipList, LiveComment},
+    data() {
+        return {
+            id: this.$route.params.id,
+            data: [],
+            showed: 10,
+            is_end: false,
+            page: 0,
+            webp_support: this.$parent.webp_support
+        }
+    },
+    mounted() {
+        document.title = 'ICU for Viewers'
+        this.$parent.loading = true;
+        this.load_more();
+        window.addEventListener('scroll', this.scrollFunc);
+    },
+    methods: {
+        scrollFunc() {
+            if (document.body.clientHeight - window.scrollY - window.innerHeight < (document.body.clientHeight / this.showed)) {
+                if (!this.is_end && !this.$parent.loading) {
+                    window.removeEventListener('scroll', this.scrollFunc);
+                    this.load_more();
+                    this.showed += 10;
+                    window.addEventListener('scroll', this.scrollFunc);
                 }
             }
+        },
+        load_more() {
+            this.$parent.loading = true;
+            this.$http
+            .get(`https://api.matsuri.icu/viewer/${this.id}?page=${++this.page}`)
+            .then(function (response) {
+                if (response.data.status === 0) {
+                    this.data = this.data.concat(response.data.data);
+                    if (response.data.data.length === 0)
+                        this.is_end = true;
+                    if (response.data.data.length > 0 && document.title === 'ICU for Viewers') document.title = this.data[0].full_comments[0].username + ' - ICU for Viewers';
+                    this.$parent.loading = false;
+                }
+            }.bind(this))
         }
     }
+}
 </script>
 
 <style scoped>
-    .comment-container {
-        padding: 5px 15px;
-    }
+.comment-container {
+    padding: 5px 15px;
+}
 </style>
