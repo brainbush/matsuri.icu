8900170f9c4ae6f1ba7c40d47c8002a820ea2db2
Cololi <gelovesky@gmail.com>
2022-09-04 22:10:03 +0800

    在新tab打开

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,7 @@
                   :key="channel.bilibili_uid"/>
         <div class="empty"></div>
         <footer class="footer mt-auto py-1">
-                <a href="https://beian.miit.gov.cn/"><span>鄂ICP备2021002054号</span></a>
+                <a href="https://beian.miit.gov.cn/" target="_blank"><span>鄂ICP备2021002054号</span></a>
         </footer>
     </div>
 </template>
